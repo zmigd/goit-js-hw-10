6987e34880ddab924d938f3afeaaff6689a96815
zmigd <zmigdalovich@gmail.com>
2025-07-13 13:33:28 +0300

    install lib, import styles, scripts

---
+++ src/js/1-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+  
